0dae2c515f13601bcf637bc33a5c31c17c6c19fd
GeckoTheWrecko <68974670+Dr-Gecko@users.noreply.github.com>
2022-06-16 16:36:46 -0700

    Update restart.js

--- Bot Code/Commands/restart.js
+++ Bot Code/Commands/restart.js
@@ -1,1 +1,10 @@
-
+module.exports = {
+    name: 'restart',
+    description: 'restarts bot',
+    execute(message, args){
+        const user = message.author;
+        if(message.member.roles.cache.has('954264080171032618')){
+            process.exit();
+        }
+    }
+}
