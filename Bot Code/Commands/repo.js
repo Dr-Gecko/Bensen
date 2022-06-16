194bb9a9a9ea253269d245ebb7dcaa8f773f467c
GeckoTheWrecko <68974670+Dr-Gecko@users.noreply.github.com>
2022-06-16 16:38:44 -0700

    Create repo.js

---
+++ Bot Code/Commands/repo.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'repo',
+    description: 'Github Repository for this bot',
+    execute(message, args){
+        message.channel.send("Repository for this bot is at: https://github.com/Dr-Gecko/Sir-Stabs-A-Lot");
+    }
+}
