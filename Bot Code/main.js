dc57f97f721dec2de89d27325d714e56cec236db
GeckoTheWrecko <68974670+Dr-Gecko@users.noreply.github.com>
2022-06-16 13:04:54 -0700

    Update main.js

--- Bot Code/main.js
+++ Bot Code/main.js
@@ -1,12 +1,37 @@
 const Discord = require('discord.js');
 
-const client = new Discord.Client();
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
+const prefix = '-';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./Commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./Commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
 
 client.once('ready', () => {
   console.log('Bot is online')
 
 });
 
-
-client.login('token')
+client.on("messageCreate", message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+    if(command === 'ping'){
+        client.commands.get('ping').execute(message, args)
+    }
+    if(command === 'author'){
+        client.commands.get('author').execute(message, args)
+    }
+    if(command === 'test'){
+        client.commands.get('test').execute(message, args)
+    }
+});
+client.login('<Your Token>')
