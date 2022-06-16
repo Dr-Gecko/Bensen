0e17f818710cfd29eb58965b861a58779bb19985
GeckoTheWrecko <68974670+Dr-Gecko@users.noreply.github.com>
2022-06-16 16:55:47 -0700

    Update startup.js

--- Bot Code/Commands/startup.js
+++ Bot Code/Commands/startup.js
@@ -1,1 +1,7 @@
-
+module.exports = {
+    name: 'startup',
+    description: 'command for when bot starts up',
+    execute(message, args){
+        message.channel.send('I am online now!');
+    }
+}
