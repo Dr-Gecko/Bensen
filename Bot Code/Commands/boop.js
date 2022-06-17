a80d6caec0ce09e4c3927b79d6bb11b55d7207d8
GeckoTheWrecko <68974670+Dr-Gecko@users.noreply.github.com>
2022-06-17 14:31:13 -0700

    Delete Bot Code/Commands directory

--- Bot Code/Commands/boop.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'boop',
-    description: 'boop the snoot',
-    execute(message, args){
-        message.channel.send('https://c.tenor.com/6XkXZxlaMD8AAAAC/boop-snoot.gif');
-    }
-}
