94967d7f7b1f2ccafcad927e54aaa4e7607115c6
GeckoTheWrecko <68974670+Dr-Gecko@users.noreply.github.com>
2022-06-17 09:50:27 -0700

    Create boop.js

---
+++ Bot Code/Commands/boop.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'boop',
+    description: 'boop the snoot',
+    execute(message, args){
+        message.channel.send('https://c.tenor.com/6XkXZxlaMD8AAAAC/boop-snoot.gif');
+    }
+}
