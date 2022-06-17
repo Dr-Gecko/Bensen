6df18dcc57021c0365950457240e11a03cf6a1c1
GeckoTheWrecko <68974670+Dr-Gecko@users.noreply.github.com>
2022-06-17 10:40:04 -0700

    Update help.js

--- Bot Code/Commands/help.js
+++ Bot Code/Commands/help.js
@@ -9,10 +9,10 @@ module.exports = {
         author: Gives my Github Profile
         test: So you can live test commands
         repo: Gives link to this repository
-        stab: Do prefix @user and it will respond with a photo and tagging the person
+        stab: Stabs tagged user
         shutdown: This will only work if you have a moderator permission and will shut down the bot
         v: Shows information about the bot
-        help: Displays this
+        h: Displays this
         `)
 
     }
