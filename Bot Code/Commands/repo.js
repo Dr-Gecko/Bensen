cdd59ed9304f8e94f3e30bfb9cffc9c7afa327f7
GeckoTheWrecko <68974670+Dr-Gecko@users.noreply.github.com>
2022-06-17 09:06:36 -0700

    Delete repo.js

--- Bot Code/Commands/repo.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'repo',
-    description: 'Github Repository for this bot',
-    execute(message, args){
-        message.channel.send("Repository for this bot is at: https://github.com/Dr-Gecko/Sir-Stabs-A-Lot");
-    }
-}
