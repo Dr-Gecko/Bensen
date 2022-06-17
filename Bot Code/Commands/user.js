c1d70385f3cc16def1cec5899b224b4088edef7d
GeckoTheWrecko <68974670+Dr-Gecko@users.noreply.github.com>
2022-06-17 15:54:14 -0700

    Update user.js

--- Bot Code/Commands/user.js
+++ Bot Code/Commands/user.js
@@ -7,5 +7,8 @@ module.exports = {
         if (mentioned){
             message.channel.send(`Username: <@${mentioned.username}> \n Tag: <@${mentioned.discriminator}> \n ID: <@${mentioned.id}> \n Bot: <@${mentioned.bot}>`)
         }
+        else{
+            message.channel.send(`Username: <@${user.username}> \n Tag: <@${user.discriminator}> \n ID: <@${user.id}> \n Bot: <@${user.bot}>`)
+        }
     }
 }
