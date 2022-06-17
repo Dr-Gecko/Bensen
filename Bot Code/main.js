03c8414b67842aba4f502a96fb79997fe85e630e
GeckoTheWrecko <68974670+Dr-Gecko@users.noreply.github.com>
2022-06-17 15:30:40 -0700

    Update main.js

--- Bot Code/main.js
+++ Bot Code/main.js
@@ -23,33 +23,43 @@ client.on("messageCreate", message =>{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
-    if(command === 'v'){
-        client.commands.get('info').execute(message, args)
-    }
-    if(command === 'ping'){
-        client.commands.get('ping').execute(message, args)
-        console.log("Ping Was Used")
-    }
-    if(command === 'pong'){
-        client.commands.get('pong').execute(message, args)
-    }
-    if(command === 'stab'){
-        client.commands.get('stab').execute(message, args)
-    }
-    if(command === 'h'){
-        client.commands.get('help').execute(message, args)
-    }
     if(command === 'boop'){
         client.commands.get('boop').execute(message, args)
     }
-    if(command === 'redpanda'){
-        client.commands.get('moshu').execute(message, args)
+    if(command === 'flip'){
+        client.commands.get('flip').execute(message, args)
     }
     if(command === 'frog'){
         client.commands.get('frog').execute(message, args)
     }
+    if(command === 'h'){
+        client.commands.get('help').execute(message, args)
+    }
+    if(command === 'hp'){
+        client.commands.get('hp').execute(message, args)
+    }
+    if(command === 'info'){
+        client.commands.get('info').execute(message, args)
+    }
+    if(command === 'ping'){
+        client.commands.get('ping').execute(message, args)
+    }else if(command === 'pong'){
+        client.commands.get('pong').execute(message, args)
+    }
     if(command === 'possum'){
         client.commands.get('possum').execute(message, args)
+    }
+    if(command === 'stab'){
+        client.commands.get('stab').execute(message, args)
+    }
+    if(command === 'redpanda'){
+        client.commands.get('moshu').execute(message, args)
+    }
+    if(command === 'user'){
+        client.command.get('user').execute(message, args)
+    }
+    if(command === 'who'){
+        client.commands.get('who').execute(message, args)
     }
 });
 client.login(token);
