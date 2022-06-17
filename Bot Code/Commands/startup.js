562101979b2ba120356d8fd73498856263258aef
GeckoTheWrecko <68974670+Dr-Gecko@users.noreply.github.com>
2022-06-16 17:53:46 -0700

    Delete startup.js

--- Bot Code/Commands/startup.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'startup',
-    description: 'command for when bot starts up',
-    execute(message, args){
-        message.channel.send('I am online now!');
-    }
-}
