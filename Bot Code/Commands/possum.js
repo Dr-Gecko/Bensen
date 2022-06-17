a3a2108efbff5c95a0c50e2f3f8c3fde86fd1e72
GeckoTheWrecko <68974670+Dr-Gecko@users.noreply.github.com>
2022-06-17 11:32:44 -0700

    Create possum.js

---
+++ Bot Code/Commands/possum.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'possum',
+    description: 'Sends a random possum video from the list below',
+    execute(message, args){
+        var links = [
+            "https://www.youtube.com/watch?v=bRIVTEBaLkA",
+            ""
+            ];
+         var randVideo = links[Math.floor(Math.random() * links.length)];
+        message.channel.send("Here is a random possum video: " + randVideo)
+    }
+}
