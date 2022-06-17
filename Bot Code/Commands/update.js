e733d1921f27c61b38fc6ec8657a8b7bcf23dc7b
GeckoTheWrecko <68974670+Dr-Gecko@users.noreply.github.com>
2022-06-17 16:36:23 -0700

    Create update.js

---
+++ Bot Code/Commands/update.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'update',
+    description: 'Displays updates',
+    execute(message, args){
+        message.channel.send(`Complete overhaul of commands and structure - 6/17/22`)
+    }
+}
