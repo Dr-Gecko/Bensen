fadd3bbba5ce82f6a6273d235fbfa4420f0bba97
GeckoTheWrecko <68974670+Dr-Gecko@users.noreply.github.com>
2022-06-17 09:10:41 -0700

    Update info.js

--- Bot Code/Commands/info.js
+++ Bot Code/Commands/info.js
@@ -6,7 +6,7 @@ module.exports = {
         Bot information: 
         Duck's Name: Bensen
         Name: Stabs-A-Lot
-        Version Name: Indrik
+        Version Name: Chimera
         Version Number: 1.0.1
         My Author: https://github.com/Dr-Gecko
         Repository for my code: https://github.com/Dr-Gecko/Sir-Stabs-A-Lot
