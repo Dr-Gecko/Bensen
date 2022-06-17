be808d9e0f817413e8c9e6b030473ff6b833d422
GeckoTheWrecko <68974670+Dr-Gecko@users.noreply.github.com>
2022-06-17 15:24:06 -0700

    Update info.js

--- Bot Code/Commands/info.js
+++ Bot Code/Commands/info.js
@@ -1,12 +1,12 @@
 module.exports = {
     name: 'info',
-    description: 'Bot info',
+    description: 'Bot information',
     execute(message, args){
         message.channel.send(`
         Bot information: 
         Duck's Name: Bensen
         Version Name: Spider Snake
-        Version Number: 1.0.2
+        Version Number: 1.0.6
         My Author: https://github.com/Dr-Gecko
         Repository for my code: https://github.com/Dr-Gecko/Sir-Stabs-A-Lot
         `)
