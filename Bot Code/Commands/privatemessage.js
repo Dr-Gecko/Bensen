2628f3ff4adc8fd194289bbcb08fe064964d0e3d
GeckoTheWrecko <68974670+Dr-Gecko@users.noreply.github.com>
2022-06-21 10:20:06 -0700

    Delete privatemessage.js

--- Bot Code/Commands/privatemessage.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'pm',
-    description: 'Private Messages Command User',
-    execute(message, args){
-        const user = message.author;
-        user.send(`Hi I am Bensen and this command is coming soon!`)
-    }
-}
