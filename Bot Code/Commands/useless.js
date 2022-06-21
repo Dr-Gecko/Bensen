0980ec8e28479129d354b5fc03519df48ffc72e0
GeckoTheWrecko <68974670+Dr-Gecko@users.noreply.github.com>
2022-06-21 11:07:08 -0700

    Delete useless.js

--- Bot Code/Commands/useless.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'useless',
-    description: 'Takes user to the useless website',
-    execute(message, args){
-        message.channel.send('https://theuselessweb.com/');
-    }
-}
