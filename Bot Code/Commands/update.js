6adccb90c5ca681873bc41d32a7b94f0ea0a86f9
GeckoTheWrecko <68974670+Dr-Gecko@users.noreply.github.com>
2022-06-21 11:07:15 -0700

    Delete update.js

--- Bot Code/Commands/update.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'update',
-    description: 'Displays updates',
-    execute(message, args){
-        message.channel.send(`Complete overhaul of commands and structure - 6/17/22`)
-    }
-}
