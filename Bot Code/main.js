fe6ede84855de8da18b542cf2bcd3df8be846431
GeckoTheWrecko <68974670+Dr-Gecko@users.noreply.github.com>
2022-06-21 10:10:48 -0700

    Update main.js

--- Bot Code/main.js
+++ Bot Code/main.js
@@ -1,10 +1,24 @@
 const Discord = require('discord.js');
 const {token} = require('./config.json');
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"] });
 const version = require('./package.json');
 const fs = require('fs');
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./Commands/').filter(file => file.endsWith('.js'));
+const { MessageEmbed } = require('discord.js');
+
+
+const exampleEmbed = new MessageEmbed()
+	    .setColor('#000000')
+	    .setTitle('Rules')
+	    .setDescription('Server Rules')
+	    .addFields(
+		    { name: 'Rule 1:', value: 'Do not be unnecessarily mean or cruel, sarcasm is allowed but make sure the other person knows its sarcastic.', inline: false },
+		    { name: 'Rule 2:', value: 'Do not be over the top vulgar, slip ups are allowed but do not be over the top.', inline: false },
+            { name: 'Rule 3:', value: 'Do not post anything you would not want a parent seeing. That includes anything NSFW. ', inline: false },
+            { name: 'Rule 4:', value: 'Act your age and do not try to upset other people, we are here to have fun not target other kids.', inline: false},
+            { name: 'Rule 5:', value: 'Do not forget to take it easy and have fun!', inline: false},
+    	    )
 
 
 const prefix = '-';
@@ -18,13 +32,18 @@ for(const file of commandFiles){
 client.once('ready', () => {
     console.log("Hi I am Bensen I am now online! Try any of my commands in any of the servers I am in \n")
 });
-
+client.on('guildMemberAdd', guildMember =>{
+    guildMember.guild.channels.cache.find('988844144749326378').send(`Welcome <@${guildMember.user.id}`)
+});
 client.on("messageCreate", message =>{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
     if(command === 'boop'){
         client.commands.get('boop').execute(message, args)
+    }
+    if(command === 'embed'){
+        message.channel.send({ embeds: [exampleEmbed] })
     }
     if(command === 'flip'){
         client.commands.get('flip').execute(message, args)
@@ -34,9 +53,6 @@ client.on("messageCreate", message =>{
     }
     if(command === 'h'){
         client.commands.get('help').execute(message, args)
-    }
-    if(command === 'hp'){
-        client.commands.get('hp').execute(message, args)
     }
     if(command === 'info'){
         client.commands.get('info').execute(message, args)
@@ -50,23 +66,11 @@ client.on("messageCreate", message =>{
     if(command === 'possum'){
         client.commands.get('possum').execute(message, args)
     }
-    if(command === 'stab'){
-        client.commands.get('stab').execute(message, args)
+    if(command === 'time'){
+        client.commands.get('time').execute(message, args)
     }
     if(command === 'redpanda'){
         client.commands.get('moshu').execute(message, args)
-    }
-    if(command === 'update'){
-        client.commands.get('update').execute(message, args)
-    }
-    if(command === 'useless'){
-        client.commands.get('useless').execute(message, args)
-    }
-    if(command === 'user'){
-        client.commands.get('user').execute(message, args)
-    }
-    if(command === 'who'){
-        client.commands.get('who').execute(message, args)
     }
 });
 client.login(token);
