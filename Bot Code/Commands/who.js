77d1c1dbdea4c40ded0577b7ba245ea5e7465b9c
GeckoTheWrecko <68974670+Dr-Gecko@users.noreply.github.com>
2022-06-21 10:20:16 -0700

    Delete who.js

--- Bot Code/Commands/who.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'who',
-    description: 'Bensen introduces himself',
-    execute(message, args){
-        message.channel.send('Hi I am Bensen! I am a WIP so please be patient if I have some bugs.');
-    }
-}
